eb6d35aa8969dfe9f4f4cccfe372f581bc8d3fb1
Magno Carneiro <magnovaldo.carneiro@gmail.com>
2021-12-29 17:36:12 -0300

    Add project files.

---
+++ Project1/Contas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contas
+{
+    public class Contas
+    {
+        public int Soma(int a, int b) { return a + b; }
+        public int Subtracao(int a, int b) { return a - b; }
+        public int Multiplicacao(int a, int b) { return a * b; }
+        public int Divisao(int a, int b) { return a / b; }
+    }
+}
